1cc11215f813d0fc4e37479ca4fb3af3589791ab
Vira Leskiv <vira777lv@gmail.com>
2018-02-06 14:28:57 +0200

    add project

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,28 @@
-'use strict';
-console.log('hello');
+(function () {
+  'use strict';
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  let recognition = new SpeechRecognition();
+  recognition.interimResults = true;
+
+  let p = document.createElement('p');
+  let speechResult = document.querySelector('.speechResult');
+  speechResult.appendChild(p);
+
+  recognition.addEventListener('result', event => {
+    let transcript = Array.from(event.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('');
+
+    p.textContent = transcript;
+
+    if (event.results[0].isFinal) {
+      p = document.createElement('p');
+      speechResult.appendChild(p);
+    }
+  });
+
+  recognition.addEventListener('end', recognition.start);
+  recognition.start();
+})();
